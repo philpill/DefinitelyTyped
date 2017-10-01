25b44731f2a2ad2b46ce6eea86419be1a4308492
philpill <github@philpill.net>
2017-10-01 12:06:41 +0100

    Common

--- types/matter-js/index.d.ts
+++ types/matter-js/index.d.ts
@@ -516,10 +516,10 @@ declare namespace Matter {
         strokeStyle?: string;
 
 
-		/*
-		 * Sets the opacity. 1.0 is fully opaque. 0.0 is fully translucent
-		 */
-		opacity?: number;
+        /*
+         * Sets the opacity. 1.0 is fully opaque. 0.0 is fully translucent
+         */
+        opacity?: number;
     }
 
     export interface IBodyRenderOptionsSprite {
@@ -1119,6 +1119,278 @@ declare namespace Matter {
          * @param {vector} position
          */
         static shift(bounds: Bounds, position: Vector): void;
+    }
+
+    export class Common {
+
+        /**
+         * Extends the object in the first argument using the object in the second argument.
+         * @method extend
+         * @param {} obj
+         * @param {boolean} deep
+         * @return {} obj extended
+         */
+        static extend(obj, deep);
+
+        /**
+         * Creates a new clone of the object, if deep is true references will also be cloned.
+         * @method clone
+         * @param {} obj
+         * @param {bool} deep
+         * @return {} obj cloned
+         */
+        static clone(obj, deep);
+
+        /**
+         * Returns the list of keys for the given object.
+         * @method keys
+         * @param {} obj
+         * @return {string[]} keys
+         */
+        static keys(obj): string[];
+
+        /**
+         * Returns the list of values for the given object.
+         * @method values
+         * @param {} obj
+         * @return {array} Array of the objects property values
+         */
+        static values(obj): any[];
+
+        /**
+         * Gets a value from `base` relative to the `path` string.
+         * @method get
+         * @param {} obj The base object
+         * @param {string} path The path relative to `base`, e.g. 'Foo.Bar.baz'
+         * @param {number} [begin] Path slice begin
+         * @param {number} [end] Path slice end
+         * @return {} The object at the given path
+         */
+        static get(obj, path, begin, end): any;
+
+        /**
+         * Sets a value on `base` relative to the given `path` string.
+         * @method set
+         * @param {} obj The base object
+         * @param {string} path The path relative to `base`, e.g. 'Foo.Bar.baz'
+         * @param {} val The value to set
+         * @param {number} [begin] Path slice begin
+         * @param {number} [end] Path slice end
+         * @return {} Pass through `val` for chaining
+         */
+        static set(obj, path, val, begin, end): any;
+
+        /**
+         * Shuffles the given array in-place.
+         * The function uses a seeded random generator.
+         * @method shuffle
+         * @param {array} array
+         * @return {array} array shuffled randomly
+         */
+        static shuffle(array): any[];
+
+        /**
+         * Randomly chooses a value from a list with equal probability.
+         * The function uses a seeded random generator.
+         * @method choose
+         * @param {array} choices
+         * @return {object} A random choice object from the array
+         */
+        static choose(choices);
+
+        /**
+         * Returns true if the object is a HTMLElement, otherwise false.
+         * @method isElement
+         * @param {object} obj
+         * @return {boolean} True if the object is a HTMLElement, otherwise false
+         */
+        static isElement(obj): boolean;
+
+        /**
+         * Returns true if the object is an array.
+         * @method isArray
+         * @param {object} obj
+         * @return {boolean} True if the object is an array, otherwise false
+         */
+        static isArray(obj): boolean;
+
+        /**
+         * Returns true if the object is a function.
+         * @method isFunction
+         * @param {object} obj
+         * @return {boolean} True if the object is a function, otherwise false
+         */
+        static isFunction(obj): boolean;
+
+        /**
+         * Returns true if the object is a plain object.
+         * @method isPlainObject
+         * @param {object} obj
+         * @return {boolean} True if the object is a plain object, otherwise false
+         */
+        static isPlainObject(obj): boolean;
+
+        /**
+         * Returns true if the object is a string.
+         * @method isString
+         * @param {object} obj
+         * @return {boolean} True if the object is a string, otherwise false
+         */
+        static isString(obj): boolean;
+
+        /**
+         * Returns the given value clamped between a minimum and maximum value.
+         * @method clamp
+         * @param {number} value
+         * @param {number} min
+         * @param {number} max
+         * @return {number} The value clamped between min and max inclusive
+         */
+        static clamp(value, min, max): number;
+
+        /**
+         * Returns the sign of the given value.
+         * @method sign
+         * @param {number} value
+         * @return {number} -1 if negative, +1 if 0 or positive
+         */
+        static sign(value): -1|1;
+
+        /**
+         * Returns the current timestamp since the time origin (e.g. from page load).
+         * The result will be high-resolution including decimal places if available.
+         * @method now
+         * @return {number} the current timestamp
+         */
+        static now(): number;
+
+        /**
+         * Returns a random value between a minimum and a maximum value inclusive.
+         * The function uses a seeded random generator.
+         * @method random
+         * @param {number} min
+         * @param {number} max
+         * @return {number} A random number between min and max inclusive
+         */
+        static random(min, max): number;
+
+        /**
+         * Converts a CSS hex colour string into an integer.
+         * @method colorToNumber
+         * @param {string} colorString
+         * @return {number} An integer representing the CSS hex string
+         */
+        static colorToNumber(colorString): number;
+
+        /**
+         * The console logging level to use, where each level includes all levels above and excludes the levels below.
+         * The default level is 'debug' which shows all console messages.
+         *
+         * Possible level values are:
+         * - 0 = None
+         * - 1 = Debug
+         * - 2 = Info
+         * - 3 = Warn
+         * - 4 = Error
+         * @property Common.logLevel
+         * @type {Number}
+         * @default 1
+         */
+        static logLevel: number;
+
+        /**
+         * Shows a `console.log` message only if the current `Common.logLevel` allows it.
+         * The message will be prefixed with 'matter-js' to make it easily identifiable.
+         * @method log
+         * @param ...objs {} The objects to log.
+         */
+        static log(objs: any);
+
+        /**
+         * Shows a `console.info` message only if the current `Common.logLevel` allows it.
+         * The message will be prefixed with 'matter-js' to make it easily identifiable.
+         * @method info
+         * @param ...objs {} The objects to log.
+         */
+        static info(objs: any);
+
+        /**
+         * Shows a `console.warn` message only if the current `Common.logLevel` allows it.
+         * The message will be prefixed with 'matter-js' to make it easily identifiable.
+         * @method warn
+         * @param ...objs {} The objects to log.
+         */
+        static warn(objs: any);
+
+        /**
+         * Returns the next unique sequential ID.
+         * @method nextId
+         * @return {Number} Unique sequential ID
+         */
+        static nextId();
+
+        /**
+         * A cross browser compatible indexOf implementation.
+         * @method indexOf
+         * @param {array} haystack
+         * @param {object} needle
+         * @return {number} The position of needle in haystack, otherwise -1.
+         */
+        static indexOf(haystack, needle): number;
+
+        /**
+         * A cross browser compatible array map implementation.
+         * @method map
+         * @param {array} list
+         * @param {function} func
+         * @return {array} Values from list transformed by func.
+         */
+        static map(list, func): any[];
+
+        /**
+         * Takes a directed graph and returns the partially ordered set of vertices in topological order.
+         * Circular dependencies are allowed.
+         * @method topologicalSort
+         * @param {object} graph
+         * @return {array} Partially ordered set of vertices in topological order.
+         */
+        static topologicalSort(graph): any[];
+
+        /**
+         * Takes _n_ functions as arguments and returns a new function that calls them in order.
+         * The arguments applied when calling the new function will also be applied to every function passed.
+         * The value of `this` refers to the last value returned in the chain that was not `undefined`.
+         * Therefore if a passed function does not return a value, the previously returned value is maintained.
+         * After all passed functions have been called the new function returns the last returned value (if any).
+         * If any of the passed functions are a chain, then the chain will be flattened.
+         * @method chain
+         * @param ...funcs {function} The functions to chain.
+         * @return {function} A new function that calls the passed functions in order.
+         */
+        static chain(): Function;
+
+        /**
+         * Chains a function to excute before the original function on the given `path` relative to `base`.
+         * See also docs for `Common.chain`.
+         * @method chainPathBefore
+         * @param {} base The base object
+         * @param {string} path The path relative to `base`
+         * @param {function} func The function to chain before the original
+         * @return {function} The chained function that replaced the original
+         */
+        static chainPathBefore(base, path, func): Function;
+
+        /**
+         * Chains a function to excute after the original function on the given `path` relative to `base`.
+         * See also docs for `Common.chain`.
+         * @method chainPathAfter
+         * @param {} base The base object
+         * @param {string} path The path relative to `base`
+         * @param {function} func The function to chain after the original
+         * @return {function} The chained function that replaced the original
+         */
+        static chainPathAfter(base, path, func): Function;
+
     }
 
     export interface ICompositeDefinition {
@@ -2351,9 +2623,149 @@ declare namespace Matter {
         /**
          * Render wireframes only
          * @type boolean
-         * @default true 
+         * @default true
          */
         wireframes?: boolean;
+
+        /**
+         *
+         * @type number
+         * @default 1
+         */
+        pixelRatio?: number;
+
+        /**
+         *
+         * @type string
+         * @default '#18181d'
+         */
+        background?: string;
+
+        /**
+         *
+         * @type string
+         * @default '#0f0f13'
+         */
+        wireframeBackground?: string;
+
+        /**
+         *
+         * @type boolean
+         * @default true
+         */
+        enabled?: boolean;
+
+        /**
+         *
+         * @type boolean
+         * @default true
+         */
+        showSleeping?: boolean;
+
+        /**
+         *
+         * @type boolean
+         * @default false
+         */
+        showDebug?: boolean;
+
+        /**
+         *
+         * @type boolean
+         * @default false
+         */
+        showBroadphase?: boolean;
+
+        /**
+         *
+         * @type boolean
+         * @default false
+         */
+        showBounds?: boolean;
+
+        /**
+         *
+         * @type boolean
+         * @default false
+         */
+        showVelocity?: boolean;
+
+        /**
+         *
+         * @type boolean
+         * @default false
+         */
+        showCollisions?: boolean;
+
+        /**
+         *
+         * @type boolean
+         * @default false
+         */
+        showSeparations?: boolean;
+
+        /**
+         *
+         * @type boolean
+         * @default false
+         */
+        showAxes?: boolean;
+
+        /**
+         *
+         * @type boolean
+         * @default false
+         */
+        showPositions?: boolean;
+
+        /**
+         *
+         * @type boolean
+         * @default false
+         */
+        showAngleIndicator?: boolean;
+
+        /**
+         *
+         * @type boolean
+         * @default false
+         */
+        showIds?: boolean;
+
+        /**
+         *
+         * @type boolean
+         * @default false
+         */
+        showShadows?: boolean;
+
+        /**
+         *
+         * @type boolean
+         * @default false
+         */
+        showVertexNumbers?: boolean;
+
+        /**
+         *
+         * @type boolean
+         * @default false
+         */
+        showConvexHulls?: boolean;
+
+        /**
+         *
+         * @type boolean
+         * @default false
+         */
+        showInternalEdges?: boolean;
+
+        /**
+         *
+         * @type boolean
+         * @default false
+         */
+        showMousePosition?: boolean;
     }
 
     /**
@@ -2373,6 +2785,23 @@ declare namespace Matter {
          * @return {render} A new renderer
          */
         static create(options: IRenderDefinition): Render;
+
+
+        /**
+         * Positions and sizes the viewport around the given object bounds.
+         * Objects must have at least one of the following properties:
+         * - `object.bounds`
+         * - `object.position`
+         * - `object.min` and `object.max`
+         * - `object.x` and `object.y`
+         * @method lookAt
+         * @param {render} render
+         * @param {object[]} objects
+         * @param {vector} [padding]
+         * @param {bool} [center=true]
+         */
+        static lookAt(render: Render, objects: any, padding?: Vector, center?: boolean): void;
+
         /**
          * Continuously updates the render canvas on the `requestAnimationFrame` event.
          * @method run
